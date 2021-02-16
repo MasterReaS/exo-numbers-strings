2c379dc05bf232b2807e0bb763285a4753515c63
MasterReaS <reas@PC_Materiel_net.localdomain>
2021-02-16 15:16:31 +0100

    convert

---
+++ convert.js
@@ -0,0 +1,11 @@
+let nb1 = 10
+let nb2 = 15
+let nb3 = 16
+let nb4 = 5005
+let nb5 = 52390903
+
+console.log(nb1.toString(16))
+console.log(nb2.toString(16))
+console.log(nb3.toString(16))
+console.log(nb4.toString(16))
+console.log(nb5.toString(16))
--- decimal.js
+++ decimal.js
@@ -6,3 +6,10 @@ let nb5 = 0xea7beef
 let nb6 = 0b1111111111111111
 let nb7 = 0777
 
+console.log(nb1.toString(10))
+console.log(nb2.toString(10))
+console.log(nb3.toString(10))
+console.log(nb4.toString(10))
+console.log(nb5.toString(10))
+console.log(nb6.toString(10))
+console.log(nb7.toString(10))
